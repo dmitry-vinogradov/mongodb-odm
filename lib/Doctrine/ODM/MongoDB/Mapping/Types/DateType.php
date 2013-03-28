dmitry-vinogradov/mongodb-odm

commit b702787869dcccfc1ab203a1616527662c3d661d

Modifying DateType to store formatted ISO 8601 date when can't be converted to a unix timestamp for MongoDate creation.

lib/Doctrine/ODM/MongoDB/Mapping/Types/DateType.php
@@ -35,18 +35,17 @@ class DateType extends Type
         if ($value === null) {
           return null;
         }
+        $timestamp = false;
         if ($value instanceof \DateTime) {
             $timestamp = $value->getTimestamp();
         }
         if (is_string($value)) {
             $timestamp = strtotime($value);
         }
-        // If we have tried to create a timestamp but got false
-        // then it is likely an invalid timestamp before the unix epoch
-        // so we can only store the original $value that is just a string
-        if (isset($timestamp) && $timestamp === false) {
-          return $value;
-        // Otherwise set $value to $timestamp to create a MongoDate instance from
+        // Could not convert date to timestamp so store ISO 8601 formatted date instead
+        if ($timestamp === false) {
+          $date = new \DateTime($value);
+          return $date->format('c');
         } else {
           $value = $timestamp;
         }

tests/Doctrine/ODM/MongoDB/Tests/Functional/DateTest.php
@@ -51,6 +51,7 @@ class DateTest extends \Doctrine\ODM\MongoDB\Tests\BaseTest
       $this->assertTrue(isset($test['createdAt']));
 
       $user = $this->dm->findOne('Documents\User', array('username' => 'datetest2'));
+      $this->assertTrue($user->getCreatedAt() instanceof \DateTime);
       $this->assertEquals('1900-01-01', $user->getCreatedAt()->format('Y-m-d'));
     }
 }

commit 7a563f7001d137d40b42fb3093aa217b4c0372eb

Throw exception in DateType when conversion to MongoDate fails
This reverts a change introduced in b702787869dcccfc1ab203a1616527662c3d661d, which would store an ISO8601 date string on error.

lib/Doctrine/ODM/MongoDB/Mapping/Types/DateType.php
@@ -44,14 +44,10 @@ class DateType extends Type
         } elseif (is_string($value)) {
             $timestamp = strtotime($value);
         }
-        // Could not convert date to timestamp so store ISO 8601 formatted date instead
         if ($timestamp === false) {
-            $date = new \DateTime($value);
-            return $date->format('c');
-        } else {
-            $value = $timestamp;
+            throw new \InvalidArgumentException(sprintf('Could not convert %s to a date value', is_scalar($value) ? '"'.$value.'"' : gettype($value)));
         }
-        return new \MongoDate($value);
+        return new \MongoDate($timestamp);
     }
 
     public function convertToPHPValue($value)